7f38ad232154dddcf700b33bd07a7f70a2378baf
Carla Kechichian <carla.kechichian@hotmail.com>
2015-04-27 18:14:17 -0700

    B.O.M.B

--- view/navigation.php
+++ view/navigation.php
@@ -4,16 +4,24 @@
     require_once(__DIR__ . "/../model/config.php");
     
 ?>
-
+<head>
+    <link rel="stylesheet" type="text/css" href="css/main2.css">
+    <link href='http://fonts.googleapis.com/css?family=Arimo' rel='stylesheet' type='text/css'>
+</head>
 <nav>
     <ul>
         <!-- create a link that point to the post file -->
-        <div>
-        	<p>
+        <div class="hello">
+        	<b>
+             <p>
         		Welcome! register or login!
-        	</p>
+        	</b>
+             </p>
         </div>
+
+        <div class="link">
          <li><a href="<?php echo $path. "login.php" ?>">Log In</a></li>
          <li><a href="<?php echo $path. "register.php" ?>">Register</a></li>
+         </div>
     </ul>
 </nav>
